5edb9f405e18b9303cb06c92735876dce516835d
yusuf <yusuferaslan@hotmail.com>
2023-08-10 15:02:30 +0300

    12.Gün sonu(Autofac,Ioc Container,FluentValidation,AOP)

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,35 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.ProductName).MinimumLength(2);
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThan(0);
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
+            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürünler A harfi ile başlamalı");
+
+        }
+        //FluentValidation tarayıcımızın diline gore farklı dillerde hatamesaji destegi veriyor. Haricinde vermek istersek WithMessage ile verilebilir.
+
+
+        //Must sart demek, uymalı demek (StartWithA)'da kendi yazacagımız metot. Bu metota uymalı.
+        //Asagidaki (string arg),  "RuleFor(p => p.ProductName).Must(StartWithA);" bu satırdaki gonderilen productname demek.
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+           
+        }
+        //return arg.StartsWith("A"); buradaki arg C# icindeki string fonksiyonu eger A ile basliyorsa true donuyor degilse false
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -18,12 +21,25 @@ namespace WebAPI
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            //Autofac, Ninject, CastleWindsor, StructureMap, LightInject, DryInject -- > IoC Container
 
-            //Bana arka planda bir referans oluþtur. Ioc bizim yerimize new liyor. Birisi senden Iproductservice isterse ona productmanager olustur onu ver demektir.
+            //Bana arka planda bir referans oluþtur. IoC bizim yerimize new liyor. Birisi senden Iproductservice isterse ona productmanager olustur onu ver demektir.
             //Eger sen bir bagýmlýlýk görürsen (productservice) bu tipte (productmanager) onun karsýlýgý budur.
             //Biri constructor da Iproductservice isterse ona arkaplanda productmanager newi ver demek.
-            builder.Services.AddSingleton<IProductService, ProductManager>();
-            builder.Services.AddSingleton<IProductDal, EfProductDal>();
+            //builder.Services.AddSingleton<IProductService, ProductManager>();
+            //builder.Services.AddSingleton<IProductDal, EfProductDal>();
+            //yukarýdaki.Netcore Ioc container yerine busines katmanýndaki autofac kullanacagýmýz icin kodlar kapatýldý.
+
+            //Asagida diyoruz ki .Net sen kendi alt yapýný kullanma IoC olarak autofac i kullan 
+            // .NetCore IoC container yerine baska bir container (autofac) kullanmak istersem asaðýdaki metotla onu tanýtmak gereklidir.
+            //autofac kod blogu baslangýcý
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+            builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+            {
+                builder.RegisterModule(new AutofacBusinessModule());
+            });
+            // autofac eklentisi kod blogu sonu
+
 
 
 
